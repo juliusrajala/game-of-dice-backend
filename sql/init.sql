f08e3c9aa086646e8bb8ca5a4c8f88d3fe041d42
Julius Rajala <juliusrajala@gmail.com>
2020-04-14 19:33:38 +0300

    Updated data-models.

--- sql/init.sql
+++ sql/init.sql
@@ -7,27 +7,38 @@ CREATE TABLE public.users (
   is_admin boolean
 );
 
+CREATE TABLE public.rooms (
+  room_id character varying(36) PRIMARY KEY,
+  owner_id character varying(36) REFERENCES public.users(user_id),
+  room_name character varying 36 not null,
+  timestamp numeric NOT NULL
+);
+
+
 CREATE TABLE public.characters (
   character_id character varying(36) PRIMARY KEY not null,
   character_name character varying(256) not null,
-  hit_points numeric,
-  armor_class numeric,
-  attack_bonus numeric,
-  damage_taken numeric,
-  fortitude numeric,
-  reflex numeric,
-  will numeric,
-  owner_id character varying(36) not null
+  hit_points numeric default 0,
+  armor_class numeric default 0,
+  attack_bonus numeric default 0,
+  damage_taken numeric default 0,
+  fortitude numeric default 0,
+  reflex numeric default 0,
+  will numeric default 0,
+  initiative numeric default 0,
+  level numeric default 0,
+  owner_id character varying(36) REFERENCES public.users(user_id)
 );
 
 CREATE TABLE public.events (
   event_id character varying(36) PRIMARY KEY not null,
   event_type character varying(25),
-  creator_id character varying(36),
+  creator_id character varying(36) REFERENCES public.users(user_id),
   timestamp numeric NOT NULL,
   description character varying(256) NOT NULL,
   rolls json,
   skill character varying(25),
   damage_type character varying(25),
-  target character varying(256)
+  target character varying(256) REFERENCES public.characters(character_id),
+  room_id character varying(36) REFERENCES public.rooms(room_id)
 );
